e619c0df5f8a54b18ee37305ef07f6a178914ca7
fakgbeng <temris123@gmail.com>
2022-10-12 17:07:40 +0100

    useContext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
@@ -27,3 +29,17 @@ const Navigation = (props) => {
 };
 
 export default Navigation;
+
+/*
+React Context
+Passing data through a lot of components via props are sometimes bad practice. In react we have a component-wide, behind-the-scenes state storage called react context.
+
+This allows us to trigger an action in that component-wide state-storage and then directly pass that to the component that is interested without building a long prop chain.
+
+So we might ask if this is doing exactly what props is doing and even better😀, then why use prop🤔. So we should know react context also have some limitations.
+
+Context limitations
+==> React context is not optimized for high frequency changes.
+==> It also shouldn't be used to replace ALL component communications and props.
+
+*/
